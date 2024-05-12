39a2fa25d4952465482e88d21d518d0a437eda6d
TiffannyVarela <alexanegas88@gmail.com>
2024-05-12 17:23:06 -0600

    test201

--- src/Pages/CrearE.jsx
+++ src/Pages/CrearE.jsx
@@ -12,12 +12,19 @@ export const CrearE = () => {
                     <div>
                     <h2>Datos Pincipales</h2>
                     <label for="nombre_empresa">Nombre Empresa:</label>
+                    <input type="text" id="nombre_empresa" name="nombre_empresa" required></input>
                     <label for="cif">CIF:</label>
+                    <input type="text" id="cif" name="cif" required></input>
                     <label for="id_director">ID del Director:</label>
-                    <label for="direccion">Direcciocuten:</label>
-                    <label for="dni">DNI:</label>
-                    <label for="dni">DNI:</label>
-                    <label for="dni">DNI:</label>
+                    <input type="text" id="id_director" name="id_director" required></input>
+                    <label for="direccion">Direcci&oacute;n:</label>
+                    <textarea name="direccion" form="basicos">Enter text here...</textarea>
+                    <label for="correo">Correo:</label>
+                    <input type="email" id="correo" name="correo" required></input>
+                    <label for="telefono">Telefono:</label>
+                    <input type="text" id="telefono" name="telefono" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required />
+                    <label for="f_creacion">Fecha de Creaci&oacute;n:</label>
+                    <input type="date" id="fecha_nac" name="fecha_nac" required></input>
                   </div>
                     </div>
                 </form>

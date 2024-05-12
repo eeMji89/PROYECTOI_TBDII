5f9e79866b8f45ff35f1a1502a15623fceb3abd2
TiffannyVarela <alexanegas88@gmail.com>
2024-05-12 16:48:06 -0600

    emp

--- src/Pages/ReadE.jsx
+++ src/Pages/ReadE.jsx
@@ -7,6 +7,16 @@ export const ReadE = () => {
                 <h2>Leer Datos Empresa</h2>
 
                 <form id="basicos">
+                  <div>
+                    <h2>Datos Pincipales</h2>
+                    <label for="nombre_empresa">Nombre Empresa:</label>
+                    <label for="cif">CIF:</label>
+                    <label for="id_director">ID del Director:</label>
+                    <label for="dni">DNI:</label>
+                    <label for="dni">DNI:</label>
+                    <label for="dni">DNI:</label>
+                    <label for="dni">DNI:</label>
+                  </div>
                     <div id="main_b">
                     </div>
                 </form>
